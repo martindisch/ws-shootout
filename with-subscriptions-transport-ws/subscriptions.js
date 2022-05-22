96a2cea89e670b0731fb03d6fb6f0f08942db2c2
Martin Disch <martindisch@gmail.com>
2022-05-22 16:12:23 +0200

    Revert everything

--- with-subscriptions-transport-ws/subscriptions.js
+++
@@ -1,9 +0,0 @@
-const SOMETHING_CHANGED_TOPIC = "something_changed";
-
-export const resolvers = {
-  Subscription: {
-    somethingChanged: {
-      subscribe: () => pubsub.asyncIterator(SOMETHING_CHANGED_TOPIC),
-    },
-  },
-};
