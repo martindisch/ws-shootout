340a160d6b5d4701620c63f72490e424d980f4c4
Martin Disch <martindisch@gmail.com>
2022-05-22 17:13:38 +0200

    Spawn multiple subscribers

--- bencher/src/main.rs
+++ bencher/src/main.rs
@@ -1,11 +1,22 @@
+use futures_util::future;
 use futures_util::stream::StreamExt;
 use tokio_tungstenite::tungstenite::Message;
 
 #[tokio::main]
 async fn main() {
-    let (mut socket, _) = tokio_tungstenite::connect_async("ws://127.0.0.1:8080")
-        .await
-        .unwrap();
+    let subscriber_count = 5;
+    let address = "ws://127.0.0.1:8080";
+
+    let join_handles: Vec<_> = (0..subscriber_count)
+        .into_iter()
+        .map(|_| tokio::spawn(subscribe(address)))
+        .collect();
+
+    future::join_all(join_handles).await;
+}
+
+async fn subscribe(address: &str) {
+    let (mut socket, _) = tokio_tungstenite::connect_async(address).await.unwrap();
 
     while let Some(Ok(Message::Text(text))) = socket.next().await {
         println!("{}", text);
