90d914eea7d6ffea83899db57b2da144087497ea
Martin Disch <martindisch@gmail.com>
2022-05-22 16:58:19 +0200

    Log client count

--- with-ws/index.js
+++ with-ws/index.js
@@ -1,18 +1,19 @@
 import { WebSocketServer } from "ws";
 
-const connections = new Set();
+const clients = new Set();
 
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (ws) => {
-  connections.add(ws);
+  clients.add(ws);
 
-  ws.on("close", () => connections.delete(ws));
+  ws.on("close", () => clients.delete(ws));
 });
 
 const periodicUpdate = () => {
   const date = new Date().toString();
-  connections.forEach((ws) => ws.send(date));
+  console.log(`Sending date to ${clients.size} clients`);
+  clients.forEach((ws) => ws.send(date));
 
   setTimeout(periodicUpdate, 1000);
 };
