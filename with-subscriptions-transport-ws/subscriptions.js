8866121ab88181feadba2903d4b480d4f0bc024d
Martin Disch <martindisch@gmail.com>
2022-05-22 16:06:36 +0200

    Add files from docs

---
+++ with-subscriptions-transport-ws/subscriptions.js
@@ -0,0 +1,9 @@
+const SOMETHING_CHANGED_TOPIC = "something_changed";
+
+export const resolvers = {
+  Subscription: {
+    somethingChanged: {
+      subscribe: () => pubsub.asyncIterator(SOMETHING_CHANGED_TOPIC),
+    },
+  },
+};
