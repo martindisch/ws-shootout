9f11281c943f4d5430d19bef7610fef2028ddd38
Martin Disch <martindisch@gmail.com>
2022-07-23 13:52:01 +0200

    Throw Swagger overboard

--- sse/with-aspnet/Program.cs
+++ sse/with-aspnet/Program.cs
@@ -3,25 +3,10 @@ using WithAspNet;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddHostedService<EventService>();
 builder.Services.AddServerSentEvents();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.MapServerSentEvents("/");
 
