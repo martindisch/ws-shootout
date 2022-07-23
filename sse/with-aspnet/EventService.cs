12b088fa046fb8e93bcf2afffbdbb5ed8e7c4e4f
Martin Disch <martindisch@gmail.com>
2022-07-23 14:12:02 +0200

    Make behavior match that of other implementations

--- sse/with-aspnet/EventService.cs
+++ sse/with-aspnet/EventService.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Lib.AspNetCore.ServerSentEvents;
 
 namespace WithAspNet;
@@ -5,17 +6,31 @@ namespace WithAspNet;
 public class EventService : BackgroundService
 {
     private readonly IServerSentEventsService _serverSentEventsService;
+    private readonly ILogger<EventService> _logger;
 
-    public EventService(IServerSentEventsService serverSentEventsService)
+    public EventService(IServerSentEventsService serverSentEventsService, ILogger<EventService> logger)
     {
         _serverSentEventsService = serverSentEventsService;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            await _serverSentEventsService.SendEventAsync("myello", stoppingToken);
+            var now = DateTime.Now;
+
+            var terminalTime = now.ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+            var eventTime = now.ToString("yyyy-mm-dd HH:MM:ss.fffffff K");
+
+            await _serverSentEventsService.SendEventAsync(eventTime, stoppingToken);
+
+            var clientCount = _serverSentEventsService.GetClients().Count;
+            _logger.LogInformation(
+                "[{time}] Sending date to {clientCount} clients",
+                terminalTime,
+                clientCount);
+
             await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
         }
     }
