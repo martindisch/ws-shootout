abd11c5855991d9b2fe8ab5b7b4544262986e1ad
Martin Disch <martindisch@gmail.com>
2022-08-28 17:05:11 +0200

    Pin CPUs, though I have no idea which strategy is best

--- tcp/with-glommio/src/main.rs
+++ tcp/with-glommio/src/main.rs
@@ -1,8 +1,9 @@
 use glommio::{LocalExecutorPoolBuilder, PoolPlacement};
 
 fn main() {
-    let handles = LocalExecutorPoolBuilder::new(PoolPlacement::Unbound(
+    let handles = LocalExecutorPoolBuilder::new(PoolPlacement::MaxSpread(
         num_cpus::get_physical(),
+        None,
     ))
     .on_all_shards(|| async move {
         let id = glommio::executor().id();
