b5d52f8eafee3f4aa635fe92ac50252690293055
Martin Disch <martindisch@gmail.com>
2022-08-28 17:15:53 +0200

    Accept load-balanced streams

--- tcp/with-glommio/src/main.rs
+++ tcp/with-glommio/src/main.rs
@@ -1,15 +1,22 @@
-use glommio::{LocalExecutorPoolBuilder, PoolPlacement};
+use glommio::{net::TcpListener, LocalExecutorPoolBuilder, PoolPlacement};
 
 fn main() {
     let handles = LocalExecutorPoolBuilder::new(PoolPlacement::MaxSpread(
         num_cpus::get_physical(),
         None,
     ))
-    .on_all_shards(|| async move {
-        let id = glommio::executor().id();
-        println!("hello from executor {}", id);
-    })
+    .on_all_shards(listen)
     .unwrap();
 
     handles.join_all();
+}
+
+async fn listen() {
+    let id = glommio::executor().id();
+    println!("Executor {id} starting");
+
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+    while let Ok(stream) = listener.accept().await {
+        println!("Accepted new stream from executor {id}");
+    }
 }
