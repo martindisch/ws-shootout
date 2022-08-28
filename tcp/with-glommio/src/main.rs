2a9479df94e005ab529b08621df0f4af452b53b4
Martin Disch <martindisch@gmail.com>
2022-08-28 17:21:41 +0200

    Extract listening into its own function

--- tcp/with-glommio/src/main.rs
+++ tcp/with-glommio/src/main.rs
@@ -1,22 +1,34 @@
-use glommio::{net::TcpListener, LocalExecutorPoolBuilder, PoolPlacement};
+use glommio::{
+    net::{TcpListener, TcpStream},
+    LocalExecutorPoolBuilder, PoolPlacement,
+};
 
 fn main() {
     let handles = LocalExecutorPoolBuilder::new(PoolPlacement::MaxSpread(
         num_cpus::get_physical(),
         None,
     ))
-    .on_all_shards(listen)
+    .on_all_shards(serve)
     .unwrap();
 
     handles.join_all();
 }
 
-async fn listen() {
+async fn serve() {
     let id = glommio::executor().id();
     println!("Executor {id} starting");
 
+    let mut streams = Vec::new();
+
+    listen(&mut streams).await;
+}
+
+async fn listen(streams: &mut Vec<TcpStream>) {
+    let id = glommio::executor().id();
+
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
     while let Ok(stream) = listener.accept().await {
+        streams.push(stream);
         println!("Accepted new stream from executor {id}");
     }
 }
